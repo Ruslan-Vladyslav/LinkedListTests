b5e15a350b80b679f7e89dab3d86db0bfd1faa0d
Ruslan-Vladyslav <rgritsai95@gmail.com>
2025-03-09 23:07:18 +0200

    Some files modified and updated

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,12 @@
+﻿
+
+public class LinkedList
+{
+    public class Node
+    {
+
+    }
+
+
+}
+
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+public class Program
+{
+    static void Main()
+    {
+        var list = new LinkedList();
+    }
+} 
